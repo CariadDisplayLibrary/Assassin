d455406b4e5e4409ac1b382cc3fb18fe10198c06
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 22:10:26 +0100

    Initial import

---
+++ Assassin.h
@@ -0,0 +1,39 @@
+#ifndef _ASSASSIN_H
+#define _ASSASSIN_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Assassin10[];
+    extern const uint8_t Assassin11[];
+    extern const uint8_t Assassin12[];
+    extern const uint8_t Assassin13[];
+    extern const uint8_t Assassin14[];
+    extern const uint8_t Assassin15[];
+    extern const uint8_t Assassin16[];
+    extern const uint8_t Assassin17[];
+    extern const uint8_t Assassin18[];
+    extern const uint8_t Assassin19[];
+    extern const uint8_t Assassin20[];
+    extern const uint8_t Assassin21[];
+    extern const uint8_t Assassin22[];
+    extern const uint8_t Assassin23[];
+    extern const uint8_t Assassin24[];
+    extern const uint8_t Assassin25[];
+    extern const uint8_t Assassin26[];
+    extern const uint8_t Assassin27[];
+    extern const uint8_t Assassin28[];
+    extern const uint8_t Assassin29[];
+    extern const uint8_t Assassin30[];
+    extern const uint8_t Assassin31[];
+    extern const uint8_t Assassin32[];
+    extern const uint8_t Assassin33[];
+    extern const uint8_t Assassin34[];
+    extern const uint8_t Assassin35[];
+    extern const uint8_t Assassin36[];
+    extern const uint8_t Assassin37[];
+    extern const uint8_t Assassin38[];
+    extern const uint8_t Assassin39[];
+    extern const uint8_t Assassin40[];
+};
+#endif
